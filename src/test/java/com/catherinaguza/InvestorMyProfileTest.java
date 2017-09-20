d721f3b44f89c7319bd9a90cf7f7505753557ee5
catherinehz <catherinaguza@gmail.com>
2017-09-20 11:29:09 +0300

    Remove InvestorDashboardPage

--- src/test/java/com/catherinaguza/InvestorMyProfileTest.java
+++ src/test/java/com/catherinaguza/InvestorMyProfileTest.java
@@ -1,6 +1,6 @@
 package com.catherinaguza;
 
-import com.catherinaguza.InvestorOfferingPage;
+import com.catherinaguza.InvestorMyProfilePage;
 import com.catherinaguza.base.BaseTest;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
@@ -10,27 +10,28 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 @RunWith(SerenityRunner.class)
-public class InvestorOfferingTest {
+public class InvestorMyProfileTest {
 
     @Managed
     protected WebDriver driver;
 
     @Steps
-    private InvestorOfferingPage.Steps user_on_investor_offering_page;
+    private InvestorMyProfilePage.Steps user_on_investor_offering_page;
 
     @Test
     public void verify_that_user_can_login_and_logout() {
 
         user_on_investor_offering_page.opens_page();
-       // user_on_investor_offering_page.wait_until_splash_is_disappear();
 
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < 10; i++) {
+            user_on_investor_offering_page.waits_until_splash_is_disappear();
             user_on_investor_offering_page.clicks_upper_login();
 
             user_on_investor_offering_page.inputs_email_string("devinvestor10@mailinator.com");
             user_on_investor_offering_page.inputs_password_string("P@ssw0rd");
             user_on_investor_offering_page.clicks_on_login_button();
 
+            user_on_investor_offering_page.waits_until_spinner_is_disappear();
             user_on_investor_offering_page.clicks_on_my_investor_profile();
             user_on_investor_offering_page.clicks_on_logout_button();
         }
